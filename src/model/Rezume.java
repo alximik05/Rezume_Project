c63047354fdbf134a354560533d2437e296668b0
alximik05 <oalximiko@gmail.com>
2014-12-18 10:56:37 +0300

    Add Rezume class

---
+++ src/model/Rezume.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Created by alximik on 18/12/14.
+ */
+public class Rezume {
+    Achievement achievement;
+    Education education;
+    Experience experience;
+    Objective objective;
+    Qualifications qualifications;
+}
